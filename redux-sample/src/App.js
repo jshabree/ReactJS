b77dd5b47916e05ad2679a17b5005d18a7c9da8a
jshabree <jshabree@emich.edu>
2020-11-01 00:29:29 -0400

    fixed prolonged error

---
+++ redux-sample/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          Hello :)
+        </header>
+      </div>
+    );
+  }  
+}
+
+export default App;
