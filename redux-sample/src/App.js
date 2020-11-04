ae5ed4f57299b58be007fe57940ae89f157002b1
jshabree <jshabree@emich.edu>
2020-11-04 11:30:09 -0500

    adding functionality

--- redux-sample/src/App.js
+++ redux-sample/src/App.js
@@ -1,23 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Form, Input, Tooltip, Button, Select, Checkbox } from "antd";
-import Icon from '@ant-design/icons';
+// import logo from './logo.svg';
+import { Form, Input, Button } from "antd";
+// import { Tooltip, Select, Checkbox } from "antd";
+// import Icon from '@ant-design/icons';
 
 import './App.css';
 
 class App extends React.Component {
   constructor(props){
     super(props);
+    this.state = {
+      username: '',
+      password: ''
+    }
   }
+
+  onTextChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState((prevState, prevProps) => {
+      return {[name] : value}
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log("username : "+this.state.username);
+    console.log("password : "+this.state.password);
+  };
+
   render(){
     return (
       <div className="App">
         <Form>
-        <Input placeholder="Enter your username"/>
-        <Input.Password placeholder="Enter password" />
-        
-        <Button type="primary">Submit</Button>
-        
+
+        <Input placeholder="Enter your username"
+        name = "username"
+        value = {this.state.username}
+        onChange = {this.onTextChange}
+        />
+
+        <Input.Password name = "password" onChange = {this.onTextChange} value={this.state.password} placeholder="Enter your password" />
+
+        <Button type="primary"  onClick={this.handleSubmit}>Submit</Button>
         
         </Form>
 
