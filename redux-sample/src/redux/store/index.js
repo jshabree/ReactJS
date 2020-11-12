9f3dad99ae0367c0e00f0eae73f2ad5af66a8632
jshabree <jshabree@emich.edu>
2020-11-12 12:36:20 -0500

    applying middleware

--- redux-sample/src/redux/store/index.js
+++ redux-sample/src/redux/store/index.js
@@ -1,12 +1,21 @@
 // store is created from reducers 
 
 import { createStore, applyMiddleware } from 'redux';
-
+import createSagaMiddleware from "redux-saga";
+import logger from "redux-logger";
 import rootSaga from "./sagas";
 import reducers from '../reducers/index';
 
+const sagaMiddleWare = createSagaMiddleware();
 
-const store = createStore(reducers);
+let middleware = [sagaMiddleWare];
 
+if(process.env.NODE_ENV === "development") {
+    middleware.push(logger);
+}
+
+const store = createStore(reducers, applyMiddleware(...middleware));
+
+sagaMiddleWare.run(rootSaga);
 
 export default store
