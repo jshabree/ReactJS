b392c530c96f91a451a21441069c33d954c2324a
jshabree <jshabree@emich.edu>
2020-11-12 13:32:21 -0500

    applying provider to app component

--- redux-sample/src/index.js
+++ redux-sample/src/index.js
@@ -2,17 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+// import * as serviceWorker from '.'
 import reportWebVitals from "./reportWebVitals";
 import "antd/dist/antd.css";
+import { Provider } from "./redux/store";
+import store from "./redux/store";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store = {store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+// serviceWorker.unregister();
 reportWebVitals();
