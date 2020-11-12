97280b656f1315c2271217b1fe1294af614f5691
jshabree <jshabree@emich.edu>
2020-11-12 10:30:04 -0500

    writing down main explanations

---
+++ redux/reducers/login.js
@@ -0,0 +1,13 @@
+// contains logic and global state of your application
+
+// create a state for login component
+
+// import types, create state object
+
+// create function to return state object
+
+// state object modifies when ever action is called
+
+// default state object
+
+// create, return reducers from reducers/index.js
