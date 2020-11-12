64d43466cf5795df7c3ea1a1c739b2a7dc53504b
jshabree <jshabree@emich.edu>
2020-11-12 12:16:45 -0500

    Importing default reducer option

--- redux/store/index.js
+++ redux/store/index.js
@@ -3,10 +3,10 @@
 import { createStore } from 'redux';
 // import createStore function
 
-import rootReducer from './reducer';
+import reducers from '../reducers/index';
 // import all reducers from reducer/index.js
 
-const store = createStore(rootReducer)
+const store = createStore(reducers);
 // create store using Redux core library
 
 export default store
