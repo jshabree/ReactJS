99ddc381a0bdcfbe3c700080f8f0c2c4a087073b
jshabree <jshabree@emich.edu>
2020-11-23 10:31:32 -0500

    functional way to wrap element

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,3 +1,5 @@
+import { compose, pipe } from 'lodash/fp';
+
 // non-functional way to trim an input between a div element
 
 let input = "  JavaScript  ";
@@ -8,11 +10,17 @@ let output = "<div>" + input.trim() + "</div>";
 // trim
 const trim = str => str.trim();
 
-//wrap inside a div element 
+// wrap inside a div element 
 
 //wrapInDiv
 
 const wrapInDiv = str => '<div>${str}</div>';
+
+// currying
+// wrap inside a span element - additional feature
+
+const wrapInSpan = str => '<span>${str}</span>';
+
 
 // function to convert string to lower case
 
@@ -20,4 +28,20 @@ const toLowerCase = str => str.toLowerCase();
 
 // first, we trim input, so we call trim function, and pass the input
 
-const result = wrapInDiv(toLowerCase(trim(input))); // read input from right to left
+// UGLY CODE ALERT !!!
+
+// const result = wrapInDiv(toLowerCase(trim(input))); // read input from right to left
+
+// effective way to write the code above
+
+// compose encompasses all the functions. does not call them, but passes a reference to them
+
+// problem : we still have to read it from right to left 
+
+// const transform = compose(wrapInDiv, toLowerCase, trim);
+
+// solution to the problem above: use "pipe"
+
+const transform = pipe(trim, toLowerCase, wrapInDiv);
+
+transform(input);
