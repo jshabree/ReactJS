e5811f79922e71b0c7d2b3071c8c2401bbd26ef6
jshabree <jshabree@emich.edu>
2020-11-23 11:48:40 -0500

    stashing

--- starter/src/index.js
+++ starter/src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { compose, pipe } from 'lodash/fp';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// non-functional way to trim an input between a div element
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let input = "  JavaScript  ";
+let output = "<div>" + input.trim() + "</div>";
+
+// functional way to trim an input between a div element
+
+// trim
+const trim = str => str.trim();
+
+// currying
+// wrap inside a span element - additional feature
+
+const wrap = (type, str) => '<type>${str}</type>';
+
+
+// function to convert string to lower case
+
+const toLowerCase = str => str.toLowerCase();
+
+// first, we trim input, so we call trim function, and pass the input
+
+// UGLY CODE ALERT !!!
+
+// const result = wrapInDiv(toLowerCase(trim(input))); // read input from right to left
+
+// effective way to write the code above
+
+// compose encompasses all the functions. does not call them, but passes a reference to them
+
+// problem : we still have to read it from right to left 
+
+// const transform = compose(wrapInDiv, toLowerCase, trim);
+
+// solution to the problem above: use "pipe"
+
+const transform = pipe(trim, toLowerCase, wrap("div"));
+
+console.log(transform(input));
